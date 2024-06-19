39bff5844dac4fd7ad71f48d6b87291db31e9a21
plazmer <48393153+getplusm@users.noreply.github.com>
2024-06-19 20:14:33 +0500

    release infinity block for Y coordinate

--- src/main/java/t/me/p1azmer/plugin/protectionblocks/utils/Cuboid.java
+++ src/main/java/t/me/p1azmer/plugin/protectionblocks/utils/Cuboid.java
@@ -1,36 +1,40 @@
 package t.me.p1azmer.plugin.protectionblocks.utils;
 
+import lombok.Getter;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+@Getter
 public class Cuboid {
 
     private final Location min;
     private final Location max;
     private final Location center;
+    private final boolean infinityY;
 
-    public Cuboid(@NotNull Location loc1, @NotNull Location loc2) {
+    public Cuboid(@NotNull Location center, @NotNull Location loc1, @NotNull Location loc2, boolean infinityY) {
+        this.infinityY = infinityY;
+        World world = loc1.getWorld();
         int minX = Math.min(loc1.getBlockX(), loc2.getBlockX());
-        int minY = Math.min(loc1.getBlockY(), loc2.getBlockY());
+        int minY = infinityY ?
+          world.getMinHeight() :
+          Math.min(loc1.getBlockY(), loc2.getBlockY());
         int minZ = Math.min(loc1.getBlockZ(), loc2.getBlockZ());
 
         int maxX = Math.max(loc1.getBlockX(), loc2.getBlockX());
-        int maxY = Math.max(loc1.getBlockY(), loc2.getBlockY());
+        int maxY = infinityY ?
+          world.getMaxHeight() :
+          Math.max(loc1.getBlockY(), loc2.getBlockY());
         int maxZ = Math.max(loc1.getBlockZ(), loc2.getBlockZ());
 
         this.min = new Location(loc1.getWorld(), minX, minY, minZ);
         this.max = new Location(loc1.getWorld(), maxX, maxY, maxZ);
-
-        double cx = minX + (maxX - minX) / 2D;
-        double cy = minY + (maxY - minY) / 2D;
-        double cz = minZ + (maxZ - minZ) / 2D;
-
-        this.center = new Location(loc1.getWorld(), cx, cy, cz);
+        this.center = center;
     }
 
     public boolean contains(@NotNull Location location) {
@@ -41,26 +45,30 @@ public class Cuboid {
         int y = location.getBlockY();
         int z = location.getBlockZ();
 
+        int minY = infinityY ? world.getMinHeight() : this.min.getBlockY();
+        int maxY = infinityY ? world.getMaxHeight() : this.max.getBlockY();
         return x >= min.getBlockX() && x <= max.getBlockX() &&
-                y >= min.getBlockY() && y <= max.getBlockY() &&
-                z >= min.getBlockZ() && z <= max.getBlockZ();
+          y >= minY && y <= maxY &&
+          z >= min.getBlockZ() && z <= max.getBlockZ();
     }
 
     @NotNull
-    public List<Block> getBlocks() {
-        List<Block> list = new ArrayList<>(this.getSize());
+    public Set<Block> getBlocks() {
+        Set<Block> list = new HashSet<>(this.getSize());
         World world = this.center.getWorld();
         if (world == null) return list;
 
+        int minY = infinityY ? world.getMinHeight() : this.min.getBlockY();
+        int maxY = infinityY ? world.getMaxHeight() : this.max.getBlockY();
+
         for (int x = this.min.getBlockX(); x <= this.max.getBlockX(); ++x) {
-            for (int y = this.min.getBlockY(); y <= this.max.getBlockY(); ++y) {
+            for (int y = minY; y <= maxY; ++y) {
                 for (int z = this.min.getBlockZ(); z <= this.max.getBlockZ(); ++z) {
                     Block blockAt = world.getBlockAt(x, y, z);
                     list.add(blockAt);
                 }
             }
         }
-
         return list;
     }
 
@@ -69,20 +77,5 @@ public class Cuboid {
         int dy = max.getBlockY() - min.getBlockY() + 1;
         int dz = max.getBlockZ() - min.getBlockZ() + 1;
         return dx * dy * dz;
-    }
-
-    @NotNull
-    public Location getMin() {
-        return this.min;
-    }
-
-    @NotNull
-    public Location getMax() {
-        return this.max;
-    }
-
-    @NotNull
-    public Location getCenter() {
-        return this.center;
     }
 }
